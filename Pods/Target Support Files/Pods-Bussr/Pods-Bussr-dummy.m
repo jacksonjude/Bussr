f759f70c39676b1b4a24b4271a7685ff0c27da5e
jacksonjude <jacksonjude@mac.com>
2023-05-31 03:36:59 -0700

    Using SPM over cocoapods

--- Pods/Target Support Files/Pods-Bussr/Pods-Bussr-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bussr : NSObject
-@end
-@implementation PodsDummy_Pods_Bussr
-@end
--- Pods/Target Support Files/Pods-BussrRecentExtension/Pods-BussrRecentExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BussrRecentExtension : NSObject
-@end
-@implementation PodsDummy_Pods_BussrRecentExtension
-@end
